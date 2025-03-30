5095b27bc9a7bbacb1937cdbc6295abe0c929238
Wagner-WR <wagner-wr@iMac-Pro-de-Wagner-WR.local>
2025-03-30 13:33:03 -0300

    Configuração para deploy no Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthProvider from './contexts/AuthProvider';
 import Login from './components/Login';
 import Register from './components/Register';
